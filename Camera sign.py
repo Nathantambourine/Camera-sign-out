18d6cadd73f8bcb60ba8db752f0b83a6669824e5
Nathantambourine <340867852@gapps.yrdsb.ca>
2023-05-25 15:21:18 -0400

    Test

---
+++ Camera sign.py
@@ -0,0 +1,14 @@
+import tkinter as tk 
+
+root = tk.Tk()
+
+root.geometry("300x300")
+root.title("Test")
+
+label = tk.Label(root, text="Hello World!", font=('Arial', 18))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, font=('Arial',22))
+textbox.pack()
+
+root.mainloop()
